302424b376a13858cc87ea76992059a2a8dbba88
itrufeng <itrufeng@gmail.com>
2013-05-29 14:37:34 +0800

    添加APPID

---
+++ bin/templates/project/__TESTING__/Classes/Model/Setting.h
@@ -0,0 +1,16 @@
+//
+//  Setting.h
+//  __TESTING__
+//
+//  Created by 张 舰 on 5/29/13.
+//
+//
+
+#ifndef __TESTING___Setting_h
+#define __TESTING___Setting_h
+
+/* appid */
+#define APP_ID 1
+
+
+#endif
