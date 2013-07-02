c668524b66da02c6941cfb014d9cb1f3ac0d9682
zhaojing <zhaojing19890531@163.com>
2013-07-02 11:06:57 +0800

    asi

--- CordovaLib/Classes/CDVGetApplicationInfo.m
+++ CordovaLib/Classes/CDVGetApplicationInfo.m
@@ -12,6 +12,8 @@
 
 #import <OpenUDID/OpenUDID.h>
 
+#import <ApplicationUnity/ASIFormDataRequest.h>
+
 #import "Setting.h"
 
 @interface CDVGetApplicationInfo ()<NSURLConnectionDelegate>
@@ -22,7 +24,7 @@
 
 @property (strong,nonatomic)ApplicationInfo *appInfo;
 @property (strong, nonatomic) NSMutableData *d;
-
+@property (strong,nonatomic)ASIFormDataRequest *request;
 
 @end
 
@@ -83,9 +85,6 @@
     
     //请求网络
     
-    NSString *url = [NSString stringWithFormat:@"%@/cloud/1/push_ios_add",API_DOMAIN];
-    
-    NSMutableURLRequest *urlRequset = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:url]];
     
     NSError *error = nil;
     
@@ -95,37 +94,72 @@
     
     NSString *jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
     
-    NSString *StrJson = [[NSString alloc]initWithFormat:@"request=%@",jsonString];
-    [urlRequset setHTTPMethod:@"POST"];
+    NSString *url = [NSString stringWithFormat:@"%@/cloud/1/push_ios_add",API_DOMAIN];
     
-    [urlRequset setHTTPBody:[StrJson dataUsingEncoding:NSUTF8StringEncoding]];
+    __block ASIFormDataRequest *request = [ASIFormDataRequest requestWithURL:[NSURL URLWithString:url]];
     
-    [NSURLConnection connectionWithRequest:urlRequset delegate:self];
+    _request = request;
+    
+    [request setRequestMethod:@"POST"];
+    
+    [request setPostValue:jsonString forKey:@"request"];
+    
+    [request setCompletionBlock:^{
+        
+        NSLog(@"请求到的数据 %@", [_request responseString]);
+    }];
+    
+    [request setFailedBlock:^{
+        NSWarn(@"网络请求失败错误状态码%d", [_request responseStatusCode]);
+    }];
+    
+    [request startAsynchronous];
+    
+        
+    
+//    NSString *url = [NSString stringWithFormat:@"%@/cloud/1/push_ios_add",API_DOMAIN];
+//    
+//    NSMutableURLRequest *urlRequset = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:url]];
+//    
+//    NSError *error = nil;
+//    
+//    NSData *jsonData =  [NSJSONSerialization dataWithJSONObject:dicSend
+//                                                        options:NSJSONWritingPrettyPrinted
+//                                                          error:&error];
+//    
+//    NSString *jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+//    
+//    NSString *StrJson = [[NSString alloc]initWithFormat:@"request=%@",jsonString];
+//    [urlRequset setHTTPMethod:@"POST"];
+//    
+//    [urlRequset setHTTPBody:[StrJson dataUsingEncoding:NSUTF8StringEncoding]];
+//    
+//    [NSURLConnection connectionWithRequest:urlRequset delegate:self];
     
 }
 
-/***********************************************************************************/
-
-#pragma mark -
-#pragma mark NSURLConnectionDelegate
-#pragma mark -
-
-/**************************************************************************************/
-
-
-- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data
-{
-    [_d appendData:data];
-}
-
-- (void)connectionDidFinishLoading:(NSURLConnection *)connection
-{
-    NSInfo(@"%@", [[NSString alloc] initWithData:_d encoding:NSUTF8StringEncoding]);
-}
-
-- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error
-{
-    NSWarn(@"网络失败送服务器需要数据%@",error);
-}
+///***********************************************************************************/
+//
+//#pragma mark -
+//#pragma mark NSURLConnectionDelegate
+//#pragma mark -
+//
+///**************************************************************************************/
+//
+//
+//- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data
+//{
+//    [_d appendData:data];
+//}
+//
+//- (void)connectionDidFinishLoading:(NSURLConnection *)connection
+//{
+//    NSInfo(@"%@", [[NSString alloc] initWithData:_d encoding:NSUTF8StringEncoding]);
+//}
+//
+//- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error
+//{
+//    NSWarn(@"网络失败送服务器需要数据%@",error);
+//}
 
 @end
